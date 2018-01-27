f6f3f8b55ddd7b5a4cfa506634ad957b3a1bd8e7
ejaguar <ejaguar211@gmail.com>
2018-01-27 10:18:21 +0900

    angular2에서 jquery 사용하기
    방법 :
    1. jquery, @types/jquery install
    
    npm install --save jquery @types/jquery
    
    2. jquery import
    
    import * as $ from 'jquery';

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,17 @@ import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { HomeComponent } from './home/home.component';
 import { JqueryComponent } from './jquery/jquery.component';
-import {AppRoutingModule} from "./app-routing.module";
+import {AppRoutingModule} from './app-routing.module';
 import {
   MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatMenuModule,
   MatToolbarModule
-} from "@angular/material";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {FlexLayoutModule} from "@angular/flex-layout";
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
 import { AngularComponent } from './angular/angular.component';
-import {UserService} from "./user.service";
-import {HttpClientModule} from "@angular/common/http";
-import {FormsModule} from "@angular/forms";
+import {UserService} from './user.service';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 import { HighlightDirective } from './highlight.directive';
 import { MydatePipe } from './mydate.pipe';
 
