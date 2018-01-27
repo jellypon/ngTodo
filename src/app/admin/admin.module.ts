ee6d5f3209e1cfd9110a93689facebc157339170
ejaguar <ejaguar211@gmail.com>
2018-01-27 11:11:14 +0900

    admin 화면 메뉴 구성

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin/admin.component';
 import { HomeComponent } from './home/home.component';
 import { NewsComponent } from './news/news.component';
-import {AdminRoutingModule} from "./admin-routing.module";
+import {AdminRoutingModule} from './admin-routing.module';
+import { MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatMenuModule,
+  MatToolbarModule } from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatButtonModule,
   ],
   declarations: [AdminComponent, HomeComponent, NewsComponent]
 })
